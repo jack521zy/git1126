5f5d00a6c62688e09db9c9a692a9efb5ee1e1802
jack <jack512512@163.com>
2022-04-13 02:04:12 +0800

    sencond commit

--- src/main/java/com/atguigu/git/Gittext.java
+++ src/main/java/com/atguigu/git/Gittext.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class Gittext {
     public static void main(String[] args) {
         System.out.println("git text");
+        System.out.println("git text1");
     }
 }
