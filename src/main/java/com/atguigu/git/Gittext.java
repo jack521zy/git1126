78e81f6ee59088a53a5e67e1f41f44a431846dac
jack <jack512512@163.com>
2022-04-13 02:07:23 +0800

    forth commit

--- src/main/java/com/atguigu/git/Gittext.java
+++ src/main/java/com/atguigu/git/Gittext.java
@@ -2,7 +2,7 @@ package com.atguigu.git;
 
 public class Gittext {
     public static void main(String[] args) {
-        System.out.println("git text");
+        System.out.println("git text0");
         System.out.println("git text1");
         System.out.println("git text2");
     }
