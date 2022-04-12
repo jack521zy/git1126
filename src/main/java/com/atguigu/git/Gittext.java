9b28d409717315e5459a10f6ee3fcc01175e7d69
jack <jack512512@163.com>
2022-04-13 02:05:52 +0800

    third commit

--- src/main/java/com/atguigu/git/Gittext.java
+++ src/main/java/com/atguigu/git/Gittext.java
@@ -4,5 +4,6 @@ public class Gittext {
     public static void main(String[] args) {
         System.out.println("git text");
         System.out.println("git text1");
+        System.out.println("git text2");
     }
 }
